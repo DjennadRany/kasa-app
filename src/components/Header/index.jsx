8893cc3ab7ac24c914bdb3743e0dff20e67f48c9
Djennad Rany <128905298+DjennadRany@users.noreply.github.com>
2024-01-20 14:06:20 +0100

    gh-page test

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,48 @@
+// Header.jsx
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <header style={headerStyle}>
+      <div style={logoStyle}>
+        {/* Ajoutez ici le code pour votre logo */}
+        <span>Logo</span>
+      </div>
+      <nav style={navStyle}>
+        <ul style={ulStyle}>
+          <li style={liStyle}><Link to="/">Accueil</Link></li>
+          <li style={liStyle}><Link to="/About">À propos</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+const headerStyle = {
+  backgroundColor: '#333',
+  color: '#fff',
+  padding: '10px',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+};
+
+const logoStyle = {
+  // Ajoutez ici le style pour le logo
+};
+
+const navStyle = {
+  // Ajoutez ici le style pour la navigation
+};
+
+const ulStyle = {
+  listStyle: 'none',
+  display: 'flex',
+};
+
+const liStyle = {
+  marginRight: '15px',
+};
+
+export default Header;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+// Home.js
+import React from 'react';
+import Banner from '../components/Banner';
+
+function Home() {
+  return (
+    <div>
+      
+      <h2>Home Page</h2>
+      <Banner text="Bienvenue sur notre site" />
+      {/* Ajoutez le contenu de votre page d'accueil ici */}
+    </div>
+  );
+}
+
+export default Home;

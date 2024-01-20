f76df90255155e5f32c33c49de644b5f45230c99
Djennad Rany <128905298+DjennadRany@users.noreply.github.com>
2024-01-20 12:17:29 +0100

    Router-installation-v1
    Mise en place du router des page Home, About, et les error 404 
    mise en place du json data 
    composant header et Footer

---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+// About.jsx
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h2>About Page</h2>
+      <p>This is the About page content.</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+// Home.js
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h2>Home Page</h2>
+      {/* Ajoutez le contenu de votre page d'accueil ici */}
+    </div>
+  );
+};
+
+export default Home;
